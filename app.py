7c28ac5369c2f776e4093a51a8d63497fdd5925f
nalinwadhwa02 <nalin.wadhwa02@gmail.com>
2022-02-21 16:43:32 +0530

    removed sqlalchemy and rebuild without it

--- app.py
+++ app.py
@@ -1,48 +1,47 @@
+import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask import render_template, request
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy import create_engine
+import psycopg2
 import random
 
-import sqlalchemy
-conn_text = 'postgresql+psycopg2://postgres:postgres@localhost/postgres'
-engine = create_engine (conn_text)
 
-db = scoped_session(sessionmaker(bind=engine))
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'
 
+conn = psycopg2.connect(
+    host="localhost",
+    database="postgres",
+    user="postgres",
+    password="postgres"
+)
 
-loginusername = "undef"
-loginuserid = "undef"
+db = conn.cursor()
+
+current_login = {
+    "username":"undef",
+    "userid":"undef"
+}
 
 @app.route('/', methods=["POST", "GET"])
 def home():
-    posts = db.execute("select u.username, t.tweet from tweets t, users u where u.userid = t.userid").fetchall()
-    users = db.execute("select username from users")
-    return render_template("index.html", posts=posts, users=users, loginuser=loginusername)
+    db.execute("select u.username, t.tweet from tweets t, users u where u.userid = t.userid;")
+    posts = db.fetchall()
+    db.execute("select username from users;")
+    users = db.fetchall()
+    return render_template("index.html", posts=posts, users=users, loginuser=current_login["username"])
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        global loginuserid
-        global loginuserid
         newusername = request.form["username"]
-        adduser = db.execute("insert into users(username) values(:username)",
-        {"username":newusername})
-        db.commit()
-        loginusername = newusername
-        loginuserid = db.execute("select userid from users where username = :username",{"username":loginusername}).fetchall()[0].userid
+        db.execute("insert into users(username) values('"+newusername+"');")
+        current_login["username"] = newusername
+        db.execute("select userid from users where username = '"+newusername+"' fetch first 1 row only;")
+        current_login["userid"] = db.fetchall()[0][0];
     return render_template("login.html")
 
 @app.route('/add', methods=["POST", "GET"])
 def add():
-    if request.method == "POST" and loginuserid != "undef":
+    if request.method == "POST" and current_login["username"]!= "undef":
         stweet = request.form["tweet"]
-        db.execute("insert into tweets(userid, tweet) values(:userid, :tweet)",
-        {"userid":loginuserid,
-        "tweet":stweet})
-        db.commit()
+        db.execute("insert into tweets(userid, tweet) values("+str(current_login["userid"])+", '"+stweet+"');")
     return render_template("add.html")
