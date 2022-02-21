c65ab95219e4283d153d58d3544371e2dd7a7f66
nalinwadhwa02 <nalin.wadhwa02@gmail.com>
2022-02-21 17:40:43 +0530

    added bootstrap, passwords and time to tweets

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import render_template, request
 import psycopg2
 import random
+import datetime
 
 
 app = Flask(__name__)
@@ -21,9 +22,13 @@ current_login = {
     "userid":"undef"
 }
 
+def get_curr_timestamp():
+    ts = datetime.datetime.now()
+    return str(ts.year)+"-"+str(ts.month)+"-"+str(ts.day)+" "+str(ts.hour)+":"+str(ts.minute)+":"+str(ts.second)
+
 @app.route('/', methods=["POST", "GET"])
 def home():
-    db.execute("select u.username, t.tweet from tweets t, users u where u.userid = t.userid;")
+    db.execute("select u.username, t.tweet, t.tweettime from tweets t, users u where u.userid = t.userid;")
     posts = db.fetchall()
     db.execute("select username from users;")
     users = db.fetchall()
@@ -32,16 +37,31 @@ def home():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        newusername = request.form["username"]
-        db.execute("insert into users(username) values('"+newusername+"');")
-        current_login["username"] = newusername
-        db.execute("select userid from users where username = '"+newusername+"' fetch first 1 row only;")
-        current_login["userid"] = db.fetchall()[0][0];
+        username = request.form["username"]
+        password = request.form["password"]
+        db.execute("select username, userid from users where username = '"+username+"' and password = '"+password+"';")
+        users = db.fetchall()
+        if(len(users) == 1):
+            current_login["username"] = users[0][0]
+            current_login["userid"] = users[0][1]
     return render_template("login.html")
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    if request.method == "POST":
+        newusername = request.form["username"]
+        newpassword = request.form["password"]
+        db.execute("insert into users(username, password) values('"+newusername+"','"+newpassword+"');")
+        conn.commit()
+    return render_template("signup.html")
 
 @app.route('/add', methods=["POST", "GET"])
 def add():
     if request.method == "POST" and current_login["username"]!= "undef":
         stweet = request.form["tweet"]
-        db.execute("insert into tweets(userid, tweet) values("+str(current_login["userid"])+", '"+stweet+"');")
+        db.execute("insert into tweets(userid, tweet, tweettime) values("
+            +str(current_login["userid"])+", '"
+            +stweet+"', '"
+            +get_curr_timestamp()+"');")
+        conn.commit()
     return render_template("add.html")
--- create_table.sql
+++ create_table.sql
@@ -1,10 +1,14 @@
+drop table tweets;
+drop table users;
 create table if not exists users (
     userid serial primary key,
+    password varchar(20) not null,
     username varchar(20) not null unique
 );
 
 create table if not exists tweets (
     tweetid serial primary key,
+    tweettime timestamp not null,
     userid integer references users (userid ),
     tweet varchar(50) not null
 );
