80d254e20d4cb534a62b8942c7b24d36eb07cefc
rakshitachoudhary <58719819+rakshitachoudhary@users.noreply.github.com>
2022-02-23 17:24:04 +0530

    Updated create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -3,21 +3,22 @@ drop table network;
 drop table users;
 
 create table if not exists users (
-    userid serial primary key,
-    password varchar(20) not null,
-    username varchar(20) not null unique
+    userid text primary key,
+    firstname text not null,
+    lastname text not null,
+    password text not null
 );
 
 create table if not exists network (
-    fr integer references users (userid),
-    fe integer references users (userid)
+    fr text references users (userid),
+    fe text references users (userid)
 );
 
 create table if not exists tweets (
     tweetid serial primary key,
+    userid text references users (userid),
     tweettime timestamp not null,
-    userid integer references users (userid),
-    tweet varchar(50) not null,
-    tweettype integer not null,
-    referencetweetid integer
+    tweet text not null,
+    response_tweets text[],
+    in_response_to_tweet text
 );
